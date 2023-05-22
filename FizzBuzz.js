a606810e4b64993e201e573b609db0fa55244c3c
awdem <wdemontmollin@gmail.com>
2023-05-22 14:53:41 +0100

    conditional challenge

---
+++ FizzBuzz.js
@@ -0,0 +1,13 @@
+const FizzBuzz = (x) => {
+  if (x % 5 === 0 && x % 3 === 0) {
+    return 'FizzBuzz';
+  } else if (x % 5 === 0) {
+    return 'Buzz';
+  } else if (x % 3 === 0) {
+    return 'Fizz';
+  } else {
+    return x;
+  }
+};
+
+module.exports = FizzBuzz;
